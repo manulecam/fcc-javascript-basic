efe15093c57b5dcc97cac946e66b0ee00ff4cb9c
manulecam <lecam.manuel@gmail.com>
2022-12-14 19:06:03 -0300

    Cuarto commit

---
+++ manipulando-objectos-complejos.js
@@ -0,0 +1,22 @@
+const myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  },
+  {
+    "artist": "Gustavo Cerati",
+    "title": "Bocanada",
+    "release_year": 1999,
+    "formats": [
+      "CD",
+      "LP"
+    ]
+  }
+];
---
+++ usa-objetos-para-hacer-bsquedas.js
@@ -0,0 +1,19 @@
+// Configuración
+function phoneticLookup(val) {
+  let result = "";
+
+  // Cambia solo el código debajo de esta línea
+  const lookup = {
+    "alpha": "Adams",
+    "bravo": "Boston",
+    "charlie": "Chicago",
+    "delta": "Denver",
+    "echo": "Easy",
+    "foxtrot": "Frank"
+  }
+  result = lookup[val];
+  // Cambia solo el código encima de esta línea
+  return result;
+}
+
+phoneticLookup("charlie");
